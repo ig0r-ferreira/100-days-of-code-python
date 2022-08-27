7e6c8d70c5d69be985863f73f0d3cf0ae45bbbe3
Igor Ferreira <igor-freitas10@hotmail.com>
2022-08-26 23:28:44 -0300

    Add score management

--- day-023/main.py
+++ day-023/main.py
@@ -10,6 +10,7 @@ def main() -> None:
     screen.setup(width=600, height=600)
     screen.tracer(0)
 
+    scoreboard = Scoreboard()
     player = Player()
     can_manager = CarManager()
 
@@ -27,11 +28,12 @@ def main() -> None:
         if not player.is_at_finish_line():
             can_manager.move_cars()
         else:
-            player.go_to_start()
+            scoreboard.increase_level()
             can_manager.level_up()
+            player.go_to_start()
 
         if can_manager.has_collision_with(player):
-            print("GAME OVER")
+            scoreboard.game_over()
             game_is_on = False
 
     screen.exitonclick()
--- day-023/scoreboard.py
+++ day-023/scoreboard.py
@@ -1,5 +1,26 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+from typing import Tuple
 
 
-class Scoreboard:
-    pass
+FONT: Tuple[str, int, str] = ("Courier", 24, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__(visible=False)
+        self.penup()
+        self.goto(-280, 250)
+        self.game_level = 1
+        self.show_level()
+
+    def show_level(self):
+        self.clear()
+        self.write(f"Level: {self.game_level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.game_level += 1
+        self.show_level()
+
+    def game_over(self):
+        self.home()
+        self.write(f"GAME OVER", align="center", font=FONT)
